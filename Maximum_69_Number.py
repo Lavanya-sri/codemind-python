764b287a4ab079d79d70e2f68e580ea08bf4d55a
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-06-29 19:57:51 +0530

    n=int(input()) arr=[] k=0 while n!=0:     r=n%10     arr.append(r)     k=k+1     n=n//10 for i in range(k-1,-1,-1):     if arr[i]==6:         arr[i]=9         break for i in range(k-1,-1,-1):     print(arr[i],end="")

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,14 +1,14 @@
 n=int(input())
-l=[]
+arr=[]
 k=0
-while n>0:
+while n!=0:
     r=n%10
-    l.append(r)
+    arr.append(r)
     k=k+1
     n=n//10
 for i in range(k-1,-1,-1):
-    if l[i]==6:
-        l[i]=9
+    if arr[i]==6:
+        arr[i]=9
         break
 for i in range(k-1,-1,-1):
-    print(l[i],end="")
+    print(arr[i],end="")
