078c26aafdac05a4b6ffdaa01d09f1e363175c07
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-07-02 20:31:44 +0530

    n=input() s=n.split() c=0 for words in s:     x=words.lower()     v=x[::-1]     if v==x:         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,9 +1,9 @@
-s=input()
-n=s.split()
-count=0
-for words in n:
-    x=words.upper()
+n=input()
+s=n.split()
+c=0
+for words in s:
+    x=words.lower()
     v=x[::-1]
-    if x==v:
-        count+=1
-print(count)
+    if v==x:
+        c+=1
+print(c)
