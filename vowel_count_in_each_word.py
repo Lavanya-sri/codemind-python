cf894470181be7c5037d91524aa2adcab398d8f6
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-07-07 10:59:52 +0530

    s=input() n=s.split() max=0 m=0 for word in n:     count=0     for ch in word:         if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U':             count=count+1     print(count,end=" ")

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,10 @@
+s=input()
+n=s.split()
+max=0
+m=0
+for word in n:
+    count=0
+    for ch in word:
+        if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U':
+            count=count+1
+    print(count,end=" ")
