77ad97beaadea3729586cc44cbb10b6ff84d9709
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-10-10 20:45:38 +0530

    n=int(input()) arr=list(map(int,input().split())) y=0 x=[] for i in range(len(arr)):     count=1     if arr[i]==-1:         continue     else:         for j in range(i+1,len(arr)):             if arr[i]==arr[j]:                 count+=1                 arr[j]=-1     if count==arr[i]:         y=1         x.append(arr[i]) if y==0:     print("-1") else:     print(*x)

---
+++ Elements_same_as_count.py
@@ -0,0 +1,20 @@
+n=int(input())
+arr=list(map(int,input().split()))
+y=0
+x=[]
+for i in range(len(arr)):
+    count=1
+    if arr[i]==-1:
+        continue
+    else:
+        for j in range(i+1,len(arr)):
+            if arr[i]==arr[j]:
+                count+=1
+                arr[j]=-1
+    if count==arr[i]:
+        y=1
+        x.append(arr[i])
+if y==0:
+    print("-1")
+else:
+    print(*x)
